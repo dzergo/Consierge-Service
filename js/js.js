07f778bf593baf6a72c124ebe272f2a9197d6a4c
dzergo <73664283+dzergo@users.noreply.github.com>
2020-11-17 16:21:54 +0200

    Consierge site v.1
    first

---
+++ js/js.js
@@ -0,0 +1,23 @@
+// slick slider
+$(document).ready(function() {
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        speed: 5000,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true
+    });
+    $('.slider2').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        speed: 5000,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        waitForAnimate: false,
+    });
+});
